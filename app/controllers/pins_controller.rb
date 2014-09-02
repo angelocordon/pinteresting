63a70222461f0655db87430ba56c957f29a65408
Angelo Cordon <angelocordon@gmail.com>
2014-09-01 18:27:02 -0700

    cleaning up Git

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,51 +1,40 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
-  
   def index
     @pins = Pin.all
   end
 
-  
   def show
   end
 
-  
   def new
     @pin = Pin.new
   end
 
-  
   def edit
   end
 
   def create
     @pin = Pin.new(pin_params)
-
-  
     if @pin.save
-      format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-    
+      redirect_to @pin, notice: 'Pin was successfully created.'
     else
-      format.html { render :new }
-    
+      render action: 'new'
     end
-    
   end
 
-  
   def update
     if @pin.update(pin_params)
-      format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
+      redirect_to @pin, notice: 'Pin was successfully updated.'
     else
-      format.html { render :edit }
+      render action: 'edit'
     end
   end
 
-  
   def destroy
     @pin.destroy
-    format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
+    redirect_to pins_url
   end
 
   private
